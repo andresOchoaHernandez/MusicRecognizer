44d57708f62773573f72c18aab001091b188c15d
andresOchoaHernandez <52919052+andresOchoaHernandez@users.noreply.github.com>
2020-06-19 13:22:19 +0200

    Add files via upload

---
+++ MusicRecognizer/src/MusicRecognizer.m
@@ -0,0 +1,22 @@
+clear;
+clc;
+
+%%Load songs in database
+
+
+%%Plot songs in database
+
+
+%%Load audio test to confront with the sogns in database
+
+
+%%Confront to with the stored songs with cross-correlation
+
+
+%%Plot the results of the confrontation
+
+
+%%Calculate/Print percentage of equality
+
+
+%%Display the song that matches
