bfbcb2fa96b62744b210e586400752cab67a2af3
andresOchoaHernandez <52919052+andresOchoaHernandez@users.noreply.github.com>
2020-06-30 11:39:54 +0200

    Add files via upload

--- src/Statistics.m
+++ src/Statistics.m
@@ -38,6 +38,35 @@
 %  e dunque campionando a 44100 Hz non vengono riconosciute. Effettuando 10
 %  secondi di registrazione a 48000 Hz le canzoni vengono riconosciute senza
 %  problemi.
+%% 1.2 Normal tests - recording from microphone,medium volume from the smartphone at 0.6 m from
+%%the microphone,10 s of recording at varying frequency (based on the mp3 freq)
+
+%test song diana : 32k Hz -> SUCCESS
+%test song freebird : 44.1 k Hz -> FAILURE
+%test song freestyler : 44.1 k Hz -> SUCCESS
+%test song funky : 44.1 k Hz -> SUCCESS
+%test song heyjoe: 44.1 k Hz -> SUCCESS
+%test song holdTheLine: 44.1 k Hz -> SUCCESS
+%test song hotelCalifornia: 44.1 k Hz -> SUCCESS
+%test song iLoveRockAndRoll: 44.1 k Hz -> SUCCESS
+%test song internetFriends: 44.1 k Hz -> SUCCESS
+%test song iWantToKnowWhatLoveIs: 44.1 k Hz -> SUCCESS
+%test song lost: 44.1 k Hz -> SUCCESS
+%test song moonRiver: 48 k Hz -> SUCCESS
+%test song never: 44.1 k Hz -> SUCCESS
+%test song rockTheCasbah: 44.1 k Hz -> SUCCESS
+%test song somebodyToLove: 48 k Hz -> SUCCESS
+%test song someWhereIBelong: 44.1 k Hz -> SUCCESS
+%test song t69: 44.1 k Hz -> SUCCESS
+%test song theRealSlimShady: 44.1 k Hz -> SUCCESS
+%test song tinyDancer: 48 k Hz -> SUCCESS
+%test song wishYouWhereHere: 44.1 k Hz -> FAILURE
+
+%SUCCESS RATE=18/20 -> 90%
+%NOTES: 
+% -freeBird e wishyouWhereHere sono molto silenziose all'inizio ed è
+%  probabilmente questo il motivo per cui non vengono riconosciute a
+%  quell'intensità di volume ed a quella distanza.
 %% 2 Normal test - Taking first 10 seconds of each song in the dataset
 
 %test song diana : SUCCESS
@@ -135,9 +164,16 @@
 %  osservato però che campionando a [44100 +- 5] Hz ottenevo un match.
 %  Questa osservazione ed il tes #1 mi portano a concludere che sarebbe ideale avere un dataset con
 %  file mp3 alla stessa frequenza in modo da aumentare le probabilità di
-%  match.
+%  match(in modo da sapere a priori a quale frequenza registrare).
 %
 % -I test #2,#3,#4 mi portano a concludere che una registrazione mediamente
 %  lunga (dai 2 ai 10 secondi) che contenga una parte non inziale di una
 %  canzone sono le modalità migliori di acquisizione di una registrazione per
 %  il riconoscimento di una canzone.
+%
+% -Il test #1.2 ed alcune osservazioni mi portano a concludere che:
+%   -Più è distante lo smartphone -> più bisogna aumentare il volume del
+%    suono e viceversa
+%   -La situazione ideale di acquisizione sarebbe quella in cui il telefono
+%   dista circa 25-30 cm dal microfono ad un volume medio
+%   -
